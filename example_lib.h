6424862aa4fc9cd99c0ab599e9eaf84e316cb4ec
Albert Palomer <albert.palo@gmail.com>
2020-11-11 08:42:46 +0100

    Added multiplatform support

--- example_lib.h
+++ example_lib.h
@@ -1,8 +1,12 @@
 
+#if defined(_WIN32) || defined(_WIN64) || defined(WINCE) || defined(__MINGW32__)
 #ifdef EXEMPLE_LIB_EXPORT
 #define EXEMPLE_LIB __declspec(dllexport)
 #else
 #define EXEMPLE_LIB __declspec(dllimport)
+#endif
+#else
+#define EXEMPLE_LIB
 #endif
 
 EXEMPLE_LIB void example();
